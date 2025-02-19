8e44e303a716bc526f5714c5e899301fea9953d6
hyerani <hran9404@naver.com>
2025-02-19 18:17:57 +0900

    feat: set firebase auth (#3)
    * feat: add firebase
    
    * feat: add react toastify, set firebase auth & login sigup component
    
    * feat: 로그인 상태 처리
    
    * feat: add logout function
    
    * feat: add context api
    
    ---------
    
    Co-authored-by: hyeran.lee <hyeran.lee@giantstepcorp.com>

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,20 +1,41 @@
-import { Link } from "react-router-dom";
-import PostList from "./PostList";
+import AuthContext from "context/AuthContext";
+import { getAuth, signOut } from "firebase/auth";
+import { app } from "firebaseApp";
+import { useContext } from "react";
+import { toast } from "react-toastify";
+
+const onSignOut = async () => {
+  const auth = getAuth(app);
+  try {
+    await signOut(auth);
+    toast.success("로그아웃 되었습니다.");
+  } catch (error: any) {
+    console.log(error);
+    toast.error(error?.code);
+  }
+};
 
 export default function Profile() {
+  // 유저정보 가져오기
+  const { user } = useContext(AuthContext);
+
   return (
     <>
       <div className="profile__box">
         <div className="flex__box-lg">
           <div className="profile__image" />
           <div>
-            <div className="profile__email">testtesttest</div>
-            <div className="profile__name">lalala</div>
+            <div className="profile__email">{user?.email}</div>
+            <div className="profile__name">{user?.displayName || "사용자"}</div>
           </div>
         </div>
-        <Link to="/" className="profile__logout">
+        <div
+          role="presentation"
+          className="profile__logout"
+          onClick={onSignOut}
+        >
           로그아웃
-        </Link>
+        </div>
       </div>
     </>
   );
--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -8,19 +8,31 @@ import ProfilePage from "pages/profile";
 import Signup from "pages/signup";
 import { Navigate, Route, Routes } from "react-router-dom";
 
-export default function Router() {
+interface RouterProps {
+  isAuthenticated: boolean;
+}
+
+export default function Router({ isAuthenticated }: RouterProps) {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/posts" element={<PostPage />} />
-        <Route path="/posts/:id" element={<PostDetailPage />} />
-        <Route path="/posts/new" element={<PostNew />} />
-        <Route path="/posts/edit/:id" element={<PostEdit />} />
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="*" element={<Navigate replace to="/" />} />
+        {isAuthenticated ? (
+          <>
+            <Route path="/" element={<Home />} />
+            <Route path="/posts" element={<PostPage />} />
+            <Route path="/posts/:id" element={<PostDetailPage />} />
+            <Route path="/posts/new" element={<PostNew />} />
+            <Route path="/posts/edit/:id" element={<PostEdit />} />
+            <Route path="/profile" element={<ProfilePage />} />
+            <Route path="*" element={<Navigate replace to="/" />} />
+          </>
+        ) : (
+          <>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="*" element={<Login />} />
+          </>
+        )}
       </Routes>
     </>
   );
